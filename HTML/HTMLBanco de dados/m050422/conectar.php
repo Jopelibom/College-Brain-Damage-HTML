5de0de4c4fa5046437bda27ce80b4e0327ec36b3
Jopelibom <82158903+Jopelibom@users.noreply.github.com>
2023-03-07 08:07:52 -0300

    Delete HTML directory

--- HTML/HTMLBanco de dados/m050422/conectar.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$pdo = new PDO('mysql:host=localhost;dbname=contatosm', 'root', Null);
-	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	?>
--- HTML/HTMLBanco de dados/m2050422/conectar.php
+++
@@ -1,5 +0,0 @@
-<?php
-	  $pdo = new PDO('mysql:host=danielgomes.pro.br;dbname=danielgo_contato', 'danielgo_alunos', 'estacio2022');
-	  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	?>
